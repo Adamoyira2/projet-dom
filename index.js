a645f9ffaf8eadcb0ace22e6112e6d750c6e3ec1
Adamoyira2 <Adamoyira2@gmail.com>
2024-04-26 09:31:04 +0200

    initializing

---
+++ index.js
@@ -0,0 +1,6 @@
+const plus = document.getElementById("btn-1")
+const moins = document.getElementById("btn-2")
+plus.addEventListener("click", () => { 
+plus.style.background= "red";
+plus.style.transition= "2s"
+})   
