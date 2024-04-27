55207330752f3b1e8b90978e23f188d8ec366543
Adamoyira2 <Adamoyira2@gmail.com>
2024-04-27 21:56:29 +0200

    initializing

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
-const plus = document.getElementById("btn-1")
-const moins = document.getElementById("btn-2")
-plus.addEventListener("click", () => { 
-plus.style.background= "red";
-plus.style.transition= "2s"
-})   
+//MODEL
+let listeArticles = [
+    {
+        nom:"casque",
+        prix:10000,
+        image:"image/image.jpg"
+        
+    },
+    {
+        nom:"montre",
+        prix:5000,
+        image:"image/1 (2).jpg"
+    },
+    {
+        nom:"écouteur",
+        prix: 2000 ,
+        image:"image/1.jpg"
+    },
+]
+
+
+//VIEW
+
+function rafraîchir() {
+    let parent = document.getElementById("accessoire")
+    for( let i in listeArticles){
+        let produit = document.createElement("div")
+        produit.setAttribute("class", "carte")
+
+        let divImg = document.createElement("div")
+        divImg.setAttribute("class", "img")
+
+        let img = document.createElement("img")
+        img.setAttribute("src",listeArticles[i].image )
+
+        let title = document.createElement("titre")
+        title.setAttribute("class", "titre")
+        title.innerText=listeArticles[i].nom
+        let coût = document.createElement("prix")
+        coût.setAttribute("class","prix")
+        coût.innerText=listeArticles[i].prix
+
+
+
+        divImg.appendChild(img)
+        divImg.appendChild(title)
+        divImg.appendChild(coût)
+
+        produit.appendChild(divImg)
+
+        parent.appendChild(produit)
+      
+    }
+}
+
+
+//CONTROLLER
+
+
+
+
+
+
+
+
+rafraîchir()
